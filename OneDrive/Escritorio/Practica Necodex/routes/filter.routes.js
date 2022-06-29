cb583537542a5f324d43696e84c189b4d45deae3
Roberto Amaya <58963198+ramaya99@users.noreply.github.com>
2022-06-29 13:59:44 -0700

    Delete OneDrive directory

--- OneDrive/Escritorio/Practica Necodex/routes/filter.routes.js
+++
@@ -1,8 +0,0 @@
-import Router from "express";
-import { filtrarPracticante } from "../controllers/filter.controller.js";
-
-const routerFilter = Router();
-
-routerFilter.get("/:termino", filtrarPracticante);
-
-export { routerFilter };
